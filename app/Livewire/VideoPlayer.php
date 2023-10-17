21488eb948596877aeaf199d78316b2968767665
정종일 <jongil@jeongjong-il-ui-MacBookPro.local>
2023-10-17 09:52:18 +0900

    pest driven practice 10

--- app/Livewire/VideoPlayer.php
+++ app/Livewire/VideoPlayer.php
@@ -2,13 +2,19 @@
 
 namespace App\Livewire;
 
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class VideoPlayer extends Component
 {
 
     public $video;
+    public $courseVideos;
+
+    public function mount(): void
+    {
+        $this->courseVideos = $this->video->course->videos;
+    }
     public function render(): View
     {
         return view('livewire.video-player');
--- resources/views/livewire/video-player.blade.php
+++ resources/views/livewire/video-player.blade.php
@@ -1,5 +1,19 @@
 <div>
-    <iframe src="https://player.vimeo.com/video/{{ $video->vimeo_id }}" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-    <h3>{{ $video->title }} ({{ $video->getReadableDuration() }})</h3>
-    <p>{{ $video->description }}</p>
+    <div>
+        <iframe src="https://player.vimeo.com/video/{{ $video->vimeo_id }}" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        <h3>{{ $video->title }} ({{ $video->getReadableDuration() }})</h3>
+        <p>{{ $video->description }}</p>
+    </div>
+    <div>
+        <ul>
+            @foreach($courseVideos as $courseVideo)
+                <li>
+                    <a href="{{ route('page.course-videos', $courseVideo) }}">
+                        {{ $courseVideo->title }}
+                    </a>
+                    {{ $courseVideo->title }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </div>
